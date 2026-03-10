044bb62ea6c7629e26542b01c3b4dfcc58223add
BaadlyList <harpreetstudying2010@gmail.com>
2026-03-10 13:25:23 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,24 @@
 const mineflayer = require('mineflayer')
 
-const bot = mineflayer.createBot({
-  host: 'Gamerboy21rip.aternos.me',
-  port: 53905,
-  username: 'Technoblade',
-  auth: 'offline'
-})
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'Gamerboy21rip.aternos.me',
+    port: 53905,
+    username: 'Technoblade',
+    auth: 'offline'
+  })
 
-bot.on('spawn', () => {
-  console.log('Bot joined server')
-})
+  bot.on('spawn', () => {
+    console.log('Bot joined server')
+  })
+
+  bot.on('error', (err) => {
+    console.log('Error:', err)
+  })
+
+  bot.on('end', () => {
+    console.log('Disconnected')
+  })
+}
+
+createBot()
